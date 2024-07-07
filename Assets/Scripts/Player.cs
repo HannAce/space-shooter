75cba8500dd062d271fc5dd2bb6b37676bd29cb1
Hannah Fisher <hannahtfisher@gmail.com>
2024-07-07 11:53:22 +0100

    Improve input for player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    int speed = 5;
+    [SerializeField]
+    private float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Set start position
         transform.position = new Vector3(0, 0, 0);
     }
 
@@ -18,23 +20,30 @@ public class Player : MonoBehaviour
         PlayerMovement();
     }
 
+    // Input for player movement
     void PlayerMovement()
     {
-        if (Input.GetKey(KeyCode.A))
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+
+        /*if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
-        else if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector3.up * Time.deltaTime * speed);
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector3.down * Time.deltaTime * speed);
-        }
+        }*/
     }
 }
