a9cd2bbb79ef3de69a1bbee0210594a962946e46
Hannah Fisher <hannahtfisher@gmail.com>
2024-07-19 12:54:01 +0100

    Added random respawn position at top of screen if enemy goes off bottom of screen

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,20 +5,27 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private float enemySpeed = 1.5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * Time.deltaTime * enemySpeed);
+        EnemyMovement();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(this.gameObject);
+    }
+
+    // Moves enemy downwards, and respawns at a random position at the top of the screen if it goes off the bottom
+    private void EnemyMovement()
+    {
+        float randomPositionX = Random.Range(-9.4f, 9.4f);
+
+        transform.Translate(Vector3.down * Time.deltaTime * enemySpeed);
+
+        if (transform.position.y < -5.5f)
+        {
+            transform.position = new Vector3(randomPositionX, 7.5f, transform.position.z);
+        }
     }
 }
