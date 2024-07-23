65fa199762b0a326d6e4aa29e14bc5e906eabf5d
Hannah Fisher <hannahtfisher@gmail.com>
2024-07-23 21:30:04 +0100

    Added Game Over text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject gameOverText;
+
     void Start()
     {
+        gameOverText.SetActive(false);
+
         Player.Instance.OnDeath += StartGameOver;
     }
 
     private void OnDestroy()
     {
+
         if (Player.Instance != null)
         {
             Player.Instance.OnDeath -= StartGameOver;
@@ -27,6 +34,8 @@ public class GameManager : MonoBehaviour
     IEnumerator StartGameOverRoutine()
     {
         Debug.Log("GAME OVER! Game will restart.");
+
+        gameOverText.SetActive(true);
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
